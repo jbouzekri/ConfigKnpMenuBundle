5d3cb8b841c743a7f5c8f0ced2795c737a9ced00
jbouzekri <jonathan.bouzekri@gmail.com>
2014-08-20 21:59:29 +0200

    cs

--- Menu/MenuBuilder.php
+++ Menu/MenuBuilder.php
@@ -202,25 +202,29 @@ class MenuBuilder
      *
      * @return null
      */
-    protected function safeUaSortItems(&$array, $cmp_function) {
-        if(count($array) < 2) {
+    protected function safeUaSortItems(&$array, $cmp_function)
+    {
+        if (count($array) < 2) {
             return;
         }
+
         $halfway = count($array) / 2;
-        $array1 = array_slice($array, 0, $halfway, TRUE);
-        $array2 = array_slice($array, $halfway, NULL, TRUE);
+        $array1 = array_slice($array, 0, $halfway, true);
+        $array2 = array_slice($array, $halfway, null, true);
 
         $this->safeUaSortItems($array1, $cmp_function);
         $this->safeUaSortItems($array2, $cmp_function);
-        if(call_user_func($cmp_function, end($array1), reset($array2)) < 1) {
+
+        if (call_user_func($cmp_function, end($array1), reset($array2)) < 1) {
             $array = $array1 + $array2;
             return;
         }
+
         $array = array();
         reset($array1);
         reset($array2);
-        while(current($array1) && current($array2)) {
-            if(call_user_func($cmp_function, current($array1), current($array2)) < 1) {
+        while (current($array1) && current($array2)) {
+            if (call_user_func($cmp_function, current($array1), current($array2)) < 1) {
                 $array[key($array1)] = current($array1);
                 next($array1);
             } else {
@@ -228,11 +232,11 @@ class MenuBuilder
                 next($array2);
             }
         }
-        while(current($array1)) {
+        while (current($array1)) {
             $array[key($array1)] = current($array1);
             next($array1);
         }
-        while(current($array2)) {
+        while (current($array2)) {
             $array[key($array2)] = current($array2);
             next($array2);
         }
